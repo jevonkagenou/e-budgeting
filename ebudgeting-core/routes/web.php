6c749548806a07069dbcf88fec95b7e2d03cf423
jevonkagenou <128831198+jevonkagenou@users.noreply.github.com>
2026-03-20 17:13:10 +0700

    feat(routes): define authenticated routes with spatie role middleware

--- ebudgeting-core/routes/web.php
+++ ebudgeting-core/routes/web.php
@@ -1,11 +1,27 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/login', function () {
-    return view('pages.auth.login');
-})->name('login');
+Route::get('/login', [AuthController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [AuthController::class, 'authenticate'])->name('login.post');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Rute Super Admin
+Route::get('/admin/dashboard', function () {
+    return view('admin.dashboard');
+})->middleware(['auth', 'role:admin']);
+
+// Rute Manajer
+Route::get('/manager/dashboard', function () {
+    return view('manager.dashboard');
+})->middleware(['auth', 'role:manager']);
+
+// Rute  Staff
+Route::get('/staff/dashboard', function () {
+    return view('staff.dashboard');
+})->middleware(['auth', 'role:staff']);
